ee54b7f2f30d7a379225ee25fcb3d90aba1caaca
TrevorRezac <trev.rezac@gmail.com>
2022-02-01 17:26:09 -0800

    food dropdown changes value in state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@
 import { useState } from 'react';
 import './App.css';
 import NameInput from './NameInput';
+import FoodDropdown from './FoodDropdown';
 
 
 function App() {
 
   const [customerName, setCustomerName] = useState('Customer');
+  const [foodId, setFoodId] = useState(1);
 
+  console.log('||', foodId);
 
   return (
     <div className="App">
       <h2>Order for: {customerName}</h2>
       <NameInput setCustomerName={setCustomerName}></NameInput>
+
+      <div className='order-form'>
+        <FoodDropdown setFoodId={setFoodId}></FoodDropdown>
+      </div>
     </div>
   );
 }
---
+++ src/FoodDropdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function FoodDropdown({ setFoodId }) {
+  function handleChange(e) {
+    setFoodId(e.target.value);
+  }
+
+  return <div>
+    <select onChange={handleChange}>
+      <option value="1">Heartburn Sando</option>
+      <option value="2">Veggie Sub</option>
+      <option value="3">Breakfast Croissant</option>
+    </select>
+  </div>;
+}

077b2417353f6cc9ceaf23df9247975e229cb863
TrevorRezac <trev.rezac@gmail.com>
2022-02-01 16:42:41 -0800

    added images and got name input to display on state change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import NameInput from './NameInput';
+
 
 function App() {
+
+  const [customerName, setCustomerName] = useState('Customer');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Order for: {customerName}</h2>
+      <NameInput setCustomerName={setCustomerName}></NameInput>
     </div>
   );
 }
